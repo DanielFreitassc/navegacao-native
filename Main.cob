ecabfd3aa301779967ca07ab599150a72c7da7ad
Daniel Freitas <129224303+DanielFreitassc@users.noreply.github.com>
2024-01-02 02:33:57 -0300

    Update Main.cob

--- Main.cob
+++ Main.cob
@@ -7,10 +7,21 @@
        01 SENHA PIC X(20).
        01 USUARIO-VALIDO PIC X(1) VALUE 'N'.
        01 RESPOSTA PIC X(20).
+       01 CONTINUAR-LOOP PIC X(1) VALUE 'Y'.
+
        PROCEDURE DIVISION.
+           PERFORM EXIBIR-TITULO.
+           PERFORM AUTENTICACAO.
+           PERFORM MENU-PRINCIPAL.
+
+           STOP RUN.
+
+       EXIBIR-TITULO.
            DISPLAY '------------------------'.
            DISPLAY 'Bem-vindo à Tela de Login'.
            DISPLAY '------------------------'.
+
+       AUTENTICACAO.
            DISPLAY 'Usuario:'.
            ACCEPT USUARIO.
            DISPLAY 'Senha:'.
@@ -21,19 +32,49 @@
            IF USUARIO-VALIDO = 'S' THEN
                DISPLAY 'Login bem-sucedido.'
                DISPLAY 'Bem-vindo: ' , USUARIO
-               DISPLAY 'Oque deseja fazer?'
-               DISPLAY '[1] Fazer Pix'
-               DISPLAY '[2] Receber Pix'
-               DISPLAY '[3] Encerrar Conta'
-               DISPLAY '[4] Sair'
-               ACCEPT RESPOSTA
            ELSE
                DISPLAY 'Usuário ou senha inválidos.'
-    
-           STOP RUN.
+               PERFORM EXIBIR-TITULO
+               PERFORM AUTENTICACAO.
+
+       MENU-PRINCIPAL.
+           PERFORM EXIBIR-MENU.
+           ACCEPT RESPOSTA.
+
+           EVALUATE TRUE
+               WHEN RESPOSTA = '1' PERFORM FAZER-PIX
+               WHEN RESPOSTA = '2' PERFORM RECEBER-PIX
+               WHEN RESPOSTA = '3' PERFORM ENCERRAR-CONTA
+               WHEN RESPOSTA = '4' PERFORM SAIR
+               WHEN OTHER PERFORM MENU-PRINCIPAL.
+
+           IF CONTINUAR-LOOP = 'Y' PERFORM MENU-PRINCIPAL.
 
        VALIDAR-USUARIO.
            IF USUARIO = 'usuario' AND SENHA = 'senha' THEN
                MOVE 'S' TO USUARIO-VALIDO
            ELSE
                MOVE 'N' TO USUARIO-VALIDO.
+
+       FAZER-PIX.
+           DISPLAY 'Opção [1] selecionada - Fazer Pix'.
+           
+
+       RECEBER-PIX.
+           DISPLAY 'Opção [2] selecionada - Receber Pix'.
+           
+
+       ENCERRAR-CONTA.
+           DISPLAY 'Opção [3] selecionada - Encerrar Conta'.
+           
+
+       SAIR.
+           DISPLAY 'Opção [4] selecionada - Sair'.
+           MOVE 'N' TO CONTINUAR-LOOP.
+
+       EXIBIR-MENU.
+           DISPLAY 'Opções de Menu:'
+           DISPLAY '[1] Fazer Pix'
+           DISPLAY '[2] Receber Pix'
+           DISPLAY '[3] Encerrar Conta'
+           DISPLAY '[4] Sair'.
