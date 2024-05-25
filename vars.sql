284937e659f7245abc9a26260ab672c38ab0242c
danielfreitassc <danielfreitassc@gmail.com>
2024-05-25 00:11:43 -0300

    Feat: conhecendo algumas vars

---
+++ vars.sql
@@ -0,0 +1,9 @@
+@@rowcount --Número de linhas processadas pelo último comando
+@@error --Número do erro reportado pelo último comando
+@@trancount --Nível de aninhamento das transações
+@@servername --Nome do servidor conectado
+@@version --Versão do SQL Server e tipo de S.O.
+@@spid --ID do processo corrente
+@@nestlevel --Níveis de aninhamento de stored procedures e triggers
+@@identity --Último valor de um identity utilizado em um insert
+@@fetch_status --Situação do último comando fetch em um cursor
